567cff0268f2e901fe4a09f358cf513fbfdb4a52
Arham Athar <arhamathar789@gmail.com>
2021-09-27 14:06:18 +0000

    dependency install

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { ToastContainer } from 'react-toastify'
+import {BrowserRouter} from 'react-router-dom'
+
+import Routes from './routes';
 
 const App = () => {
     return (
-        <div>
-            App
-        </div>
+        <BrowserRouter>
+            <Routes />
+            <ToastContainer position='bottom-left' />
+        </BrowserRouter>
     )
 }
 
