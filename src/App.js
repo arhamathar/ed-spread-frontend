6c183435efa010815cea3e1ed95ca72d14aa4b08
samarimam <samarimam78@gmail.com>
2021-09-30 21:03:23 +0530

    navbar footer added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import { ToastContainer } from 'react-toastify'
-import {BrowserRouter} from 'react-router-dom'
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
+import Footer from "./components/footer";
+import Navba from "./components/navbar";
 
-import Routes from './routes';
+import Routes from "./routes";
 
 const App = () => {
     return (
         <BrowserRouter>
+            <Navba />
             <Routes />
             <ToastContainer />
+            <Footer />
         </BrowserRouter>
-    )
-}
+    );
+};
 
-export default App
+export default App;
