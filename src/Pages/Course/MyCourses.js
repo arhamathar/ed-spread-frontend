1146e81c926d50d7e30a201538e9a074c516808a
arhamathar <arhamathar789@gmail.com>
2021-11-29 11:30:15 +0530

    auth token added to mycourse page

--- src/Pages/Course/MyCourses.js
+++ src/Pages/Course/MyCourses.js
@@ -1,53 +1,54 @@
-import React,{ useState, useEffect, useContext } from 'react'
+import React, { useState, useEffect, useContext, useCallback } from 'react';
 
-import useHttp from '../../hooks/useAuth';
+import useHttp from '../../hooks/useHttp';
 import { AuthContext } from '../../context/authContext';
 import CourseCard from '../../components/Cards/courseCard';
 
-const MyCourse = () => {    
+const MyCourse = () => {
     const auth = useContext(AuthContext);
 
     const { sendRequest } = useHttp();
 
     const [courses, setCourses] = useState([]);
 
-    const getMyCourses = async() => {
+    const getMyCourses = useCallback(async () => {
         try {
             const resp = await sendRequest(
                 `${process.env.REACT_APP_BACKEND_URL_PROD}/api/course/my-course/${auth.userId}`,
                 'GET',
                 null,
-                '/my-courses'
+                '/my-courses',
+                {
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + auth.token,
+                }
             );
             setCourses(resp.courses);
         } catch (e) {}
-    }
+    }, [auth.token, auth.userId]);
 
     useEffect(() => {
-        getMyCourses()
-    }, [courses])
+        getMyCourses();
+    }, [getMyCourses]);
 
     return (
         <>
-            <div  className='courseh1'>
+            <div className="courseh1">
                 <h1>Your Purchases</h1>
             </div>
-            <div className='courses'>
+            <div className="courses">
                 {courses.length > 0 ? (
                     courses.map((course) => (
-                        <CourseCard
-                            key={course._id}
-                            course={course}
-                        />
+                        <CourseCard key={course._id} course={course} />
                     ))
                 ) : (
-                    <div className='jumbotron-fluid'>
+                    <div className="jumbotron-fluid">
                         You have not purchased any course, Buy One Now :)
                     </div>
                 )}
             </div>
         </>
-    )
-}
+    );
+};
 
-export default MyCourse
+export default MyCourse;
